a66700549e310919a6e7a91580e604fb7898d406
alex2000d <demarcoalessandro777@gmail.com>
2024-06-25 16:41:07 +0200

    [UPD] added the calc for the final ticket price

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 let km = prompt("inserisci quanti km vuoi percorrere");
-
 let eta = prompt("inserisci la tua eta");
-
 
 let prezzo_biglietto = 0.21 * km;
  console.log(prezzo_biglietto);
+ let perc_sconto = 0;
 
-if (eta < 18){
-    sconto = 20;
+
+if (eta < 18) {
+    perc_sconto = 20;
 }
-else if (eta > 65){
-    sconto = 40;
+else if (eta > 65) {
+    perc_sconto = 40;
 }
+
+if (perc_sconto != 0){
+     let sconto = prezzo_biglietto * perc_sconto / 100;
+     prezzo = prezzo_biglietto - sconto;
+}
+
+
+console.log(prezzo);
